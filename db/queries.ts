a982d3f5e6e77731d802026865c3e2333e0765b7
Khanh Phan <khanhphanba0123@gmail.com>
2025-01-10 13:35:49 +0700

    commit

--- db/queries.ts
+++ db/queries.ts
@@ -1,5 +1,26 @@
 import {cache} from "react";
+
 import db from "@/db/drizzle";
+import { auth } from "@clerk/nextjs/server";
+import { userProgress } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        },
+    });
+
+    return data;
+});
 
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
